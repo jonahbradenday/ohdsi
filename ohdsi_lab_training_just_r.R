d8554bd4959484e61b10d2ed758fe93cf78f3638
Bradenday <j.bradenday@northeastern.edu>
2025-01-31 11:02:29 -0500

    edits to cohort creation scripts and new cohort description and analysis script

--- ohdsi_lab_training_just_r.R
+++ ohdsi_lab_training_just_r.R
@@ -1,5 +1,5 @@
-#1. Install the necessary packages. 
-#If you have already installed the following packages, skip to step 2. 
+#1. Install the necessary packages. Because ohdsilab has not been added to CRAN,
+#renv:install() is used instead of install.packages()
 renv::upgrade()
 renv::install(c("roux-ohdsi/ohdsilab","tidyverse", "DatabaseConnector", 
                 "keyring", "CohortGenerator"))
@@ -11,25 +11,21 @@ library(DatabaseConnector)
 library(keyring)
 library(CohortGenerator)
 
-#3. Set your database credentials.
-#When you run each of the following four lines of code, a popup will appear, 
-#asking for you to input the credential. 
-#Your credentials can be found in the OHDSI Lab workspace email you received 
-#after creating your workspace.
+#3. Set your database credentials. You credentials can be found in the 
+#"Workspace Details" email you received after you created your workspace.
 keyring::key_set("db_username")
 keyring::key_set("db_password")
 
-#4. Create the connection
+#4. Connect to the database.
 con =  DatabaseConnector::connect(
   dbms = "redshift",
   server = "ohdsi-lab-redshift-cluster-prod.clsyktjhufn7.us-east-1.redshift.amazonaws.com/ohdsi_lab",
   port = 5439,
   user = keyring::key_get("db_username"),
   password = keyring::key_get("db_password"),
-  pathToDriver = "D:/Users/j.bradenday/Documents/jdbc_driver"
-)
+  pathToDriver = "D:/Users/j.bradenday/Documents/jdbc_driver")
 
-#5. Assign the schema information to easy-to-reference variables.
+#5. Assign variables to the synpuf database schema and your personal schema.
 synpuf_schema = "omop_cdm_synpuf_110k_531"
 write_schema = paste0("work_", keyring::key_get("db_username"))
 
@@ -38,7 +34,8 @@ options(con.default.value = con)
 options(schema.default.value = synpuf_schema)
 options(write_schema.default.value = write_schema)
 
-#Filter for entry event
+#7. #The human readable description of the cohort designed here can be found in 
+#cohort_description.rmd.Filter the synpuf data for the entry event.
 entry_event <- tbl(con, inDatabaseSchema(synpuf_schema, 
                                          "condition_occurrence")) |>
   filter(condition_concept_id %in% c(45766052,4193704,3191208,3193274,36684827,
@@ -47,18 +44,21 @@ entry_event <- tbl(con, inDatabaseSchema(synpuf_schema,
                                      45757474,4304377)) |>
   select(person_id, entry_date = condition_start_date)
 
-#Filter for inclusion criteria
+#8. Filter the synpuf data for inclusion criteria. If more inclusion criteria are
+#needed, use naming scheme inclusion_criteria_1, inclusion_criteria_2, etc.
 inclusion_criteria <- tbl(con, inDatabaseSchema(synpuf_schema,
                                                  "drug_exposure")) |>
   filter(drug_concept_id == 40164929) |>
   select(person_id, inclusion_date = drug_exposure_start_date)
 
-#create table for observation period
+#9. Create a table for observation period. This specific cohort definition uses 
+#observation_period_end_date as the cohort_end_date for each person included.
 observation_period <-(tbl(con, inDatabaseSchema(synpuf_schema, 
                                                 "observation_period")))
 
-#Filter for both entry event and inclusion criteria and add cohort end date
-cohort <- entry_event |>
+#10. Join entry_event with inclusion_criteria, filtering for  rows that 
+#satisfy date inclusion criteria
+cohort_r <- entry_event |>
   inner_join(inclusion_criteria, by = "person_id") |>
   filter(inclusion_date >= entry_date, 
          inclusion_date <= dbplyr::sql("DATEADD(day, 30, entry_date)")) |>
@@ -70,3 +70,9 @@ cohort <- entry_event |>
   summarise(cohort_start_date = min(cohort_start_date),
     cohort_end_date = max(cohort_end_date)) |>
   ungroup()
+
+#11. After running the above code, your cohort should be nearly identical to the 
+#one generated using ATLAS in "ohdsi_lab_training_r_and_ATLAS.R" You can check 
+#by running the comparing the output of the following tally with the output of 
+#step 15 in "ohdsi_lab_training_r_and_ATLAS.R".
+tally(cohort_r)
--- ohdsi_lab_training_r_and_atlas.R
+++ ohdsi_lab_training_r_and_atlas.R
@@ -1,5 +1,5 @@
-#1. Install the necessary packages. 
-#If you have already installed the following packages, skip to step 2. 
+#1. Install the necessary packages. Because ohdsilab and ROhdsiWebApi have not 
+#been added to CRAN, renv:install() is used instead of install.packages()
 renv::upgrade()
 renv::install(c("roux-ohdsi/ohdsilab","tidyverse", "DatabaseConnector", 
                 "keyring", "OHDSI/RohdsiWebApi", "CohortGenerator"))
@@ -12,17 +12,15 @@ library(keyring)
 library(ROhdsiWebApi)
 library(CohortGenerator)
 
-#3. Set your database and ATLAS login credentials.
-#When you run each of the following four lines of code, a popup will appear, 
-#asking for you to input the credential. 
-#Your credentials can be found in the OHDSI Lab workspace email you received 
-#after creating your workspace.
+#3. Set your database and ATLAS credentials. You credentials can be found in the 
+#"Workspace Details" email you received after you created your workspace.
 keyring::key_set("db_username")
 keyring::key_set("db_password")
 keyring::key_set("atlas_username")
 keyring::key_set("atlas_password")
 
-#4. Create the connection details
+#4. Create the connection details. These details be used as arguments in later 
+#functions.
 connectionDetails <- createConnectionDetails(dbms = "redshift",
                                              server = "ohdsi-lab-redshift-cluster-prod.clsyktjhufn7.us-east-1.redshift.amazonaws.com/ohdsi_lab",
                                              port = 5439,
@@ -30,7 +28,8 @@ connectionDetails <- createConnectionDetails(dbms = "redshift",
                                              password = keyring::key_get("db_password"),
                                              pathToDriver = "D:/Users/j.bradenday/Documents/jdbc_driver")
 
-#5. Assign the schema and atlas information to easy-to-reference variables.
+#5. Assign variables to the atlas url, the synpuf database schema and your 
+#personal schema.
 atlas_url = "https://atlas.roux-ohdsi-prod.aws.northeastern.edu/WebAPI"
 synpuf_schema = "omop_cdm_synpuf_110k_531"
 write_schema = paste0("work_", keyring::key_get("db_username"))
@@ -41,34 +40,37 @@ options(schema.default.value = synpuf_schema)
 options(write_schema.default.value = write_schema)
 
 
-#7. Connect to ATLAS
+#7. Connect to ATLAS.
 ROhdsiWebApi::authorizeWebApi(atlas_url, 
                               authMethod = "db", 
                               webApiUsername = keyring::key_get("atlas_username"), 
                               webApiPassword = keyring::key_get("atlas_password"))
 
-#8. Identify the ATLAS cohort definition your want to use.
+#8. Identify the ATLAS cohort definition your want to use. 
+#The human readable description of this cohort can be found in 
+#cohort_description.rmd.
 cohortId <- 4675
 
 #9. Pull the cohort definition from ATLAS
 cohortDefinitionSet <- ROhdsiWebApi::exportCohortDefinitionSet(baseUrl = atlas_url,
                                                                cohortIds = cohortId)
-#10. Name your cohort
+#10. Name the cohort tables
 cohortTableNames <- getCohortTableNames(cohortTable = "synpuf4675")
 
-#11. Create empty tables in the white_schema cohort table
+#11. Create empty tables in your personal schema using the naming convention
+#designated in the last step.
 createCohortTables(connectionDetails = connectionDetails,
                    cohortTableNames = cohortTableNames,
                    cohortDatabaseSchema = write_schema)
 
-#12. Add everyone who fits cohort definition in synpuf to cohort table
+#12. Apply cohort definition criteria to synpuf database.
 cohortsGenerated <- generateCohortSet(connectionDetails = connectionDetails,
                                       cdmDatabaseSchema = synpuf_schema,
                                       cohortDatabaseSchema = write_schema,
                                       cohortTableNames = cohortTableNames,
                                       cohortDefinitionSet = cohortDefinitionSet)
 
-#13. Connect to the database
+#13. Connect to the database.
 con =  DatabaseConnector::connect(
   dbms = "redshift",
   server = "ohdsi-lab-redshift-cluster-prod.clsyktjhufn7.us-east-1.redshift.amazonaws.com/ohdsi_lab",
@@ -77,78 +79,12 @@ con =  DatabaseConnector::connect(
   password = keyring::key_get("db_password"),
   pathToDriver = "D:/Users/j.bradenday/Documents/jdbc_driver")
 
-#14. Filter person table for only the people in your cohort
-#14.1 pull data from your new cohort table
-cohort <- tbl(con, inDatabaseSchema(write_schema, "synpuf4669")) |>
+#14. Create a table containing your new cohort.
+cohort_atlas <- tbl(con, inDatabaseSchema(write_schema, "synpuf4675")) |>
   select(person_id = subject_id, cohort_start_date, cohort_end_date)
 
-#14.2 join to the person table to get information about demographics, calculate age 
-#at cohort entry
-demographics <- cohort |> 
-  omop_join("person", type = "inner", by = "person_id") |> 
-  select(person_id, cohort_start_date, cohort_end_date, year_of_birth, 
-         gender = gender_concept_id, race = race_concept_id, 
-         ethnicity = ethnicity_concept_id) |> 
-  mutate(age_at_entry = year(cohort_start_date) - year_of_birth)
-
-#15. join to the drug_exposure table to get information about drugs
-drugs <- demographics |>
-  omop_join("drug_exposure", type = "left", by = "person_id") |>
-  select(person_id, year_of_birth, gender, race, ethnicity, 
-         concept_id = drug_concept_id, drug_exposure_start_date, 
-         drug_exposure_end_date)
-
-#16. join to the concept table to get human readable drug names
-drugs <- drugs |>
-  omop_join("concept", type = "left", by = "concept_id") |>
-  select(person_id, year_of_birth, gender, race, ethnicity, 
-         concept_id, drug = concept_name, 
-         drug_exposure_start_date, drug_exposure_end_date)
-
-#17. Collect your joined table into a dataframe
-drugs_df <- drugs |> dbi_collect()
-
-#18. Replace gender, race, and ethnicity values with human readable values
-drugs_df <- drugs_df %>%
-  mutate(gender = case_when(gender == 8507 ~ "Male",
-                            gender == 8532 ~ "Female"),
-         race = case_when(race == 0 ~ NA,
-                          race == 8516 ~ "Black",
-                          race == 8527 ~ "White"),
-         ethnicity = case_when(ethnicity == 38003563 ~ "Hispanic or Latino",
-                               ethnicity == 38003564 ~ "Not Hispanic or Latino"))
-
-#19. Calculate prevalence rates of each drug
-drug_prevalence <- drugs_df %>%
-  count(drug) %>%         
-  mutate(prop = prop.table(n))
-
-#20. How many drugs per person per gender?
-avg_drugs_by_gender <- drugs_df %>%
-  group_by(gender, person_id) %>%
-  summarise(num_drugs = n(), .groups = "drop") %>%
-  group_by(gender) %>%
-  summarise(avg_drugs = mean(num_drugs), .groups = "drop")
-
-#21. How did drug exposure change over time?
-drugs_per_date <- drugs_df %>%
-  group_by(drug_exposure_start_date) %>%
-  summarize(person_count = n_distinct(person_id))
-
-ggplot(drugs_per_date) +
-  geom_line(aes(x = drug_exposure_start_date, y = person_count))
-
-#22 What percentage of each race has been prescribed drug x:
-#Metformin hydrochloride 500 MG Oral Tablet (e.g., white drug x users/white total)
-total_persons <- n_distinct(drugs_df$person_id)
-
-metformin_users <- drugs_df %>%
-  filter(drug == "Metformin hydrochloride 500 MG Oral Tablet") %>%
-  group_by(race) %>%
-  summarize(person_count = n_distinct(person_id), .groups = "drop") %>%
-  mutate(proportion = person_count/total_persons)
-
-ggplot(metformin_users) +
-  geom_bar(aes(x = race, y = proportion, fill = race), stat = "identity") +
-  labs(x = "Race", y = "Percentage of Race", 
-       title = "Proportion of Each Race Taking Metformin")
+#15. After running the above code, your cohort should be nearly identical to the 
+#one generated using R alone in "ohdsi_lab_training_r_and_ATLAS.R" You can check 
+#by comparing the output of the following tally with the output of 
+#step 11 in "ohdsi_lab_training_just_r.R".
+tally(cohort_atlas)
